9c8a500a83a54ad64327ea6b23f583afbea3d09e
Eddie Almada <74732394+EddieAlmada@users.noreply.github.com>
2023-06-15 20:29:09 -0600

    Update streamlit_app.py
    Adding try-except conditions

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,18 +27,28 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-# Adding text input box
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
+try:
+  # Adding text input box
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    streamlit.write('The user entered ', fruit_choice)
+ 
+except URLError as e:
+  streamlit.error()
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
-# Normalize data within JSON object
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# Display Normalized data
-streamlit.dataframe(fruityvice_normalized)
+
+
+
+
 
 streamlit.stop()
 
